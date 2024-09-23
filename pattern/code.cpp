a2ab7a21249048b23a91c06e32ed037d958b0556
sb-sagor <sheikhsagor725@gmail.com>
2024-09-23 14:05:52 +0600

    pattern

---
+++ pattern/code.cpp
@@ -0,0 +1,17 @@
+//This is an increament pattern.
+#include<iostream>
+using namespace std;
+int main(){
+   int n=5;
+ int num=1;
+   
+     for(int i=0;i<n;i++){
+         
+        for(int j=i+1;j>0;j--){
+            cout<<num << " ";
+           num++;
+        }
+         
+        cout<<endl;
+     }
+}
