601d7f858089002168cc3249bd2ffdb7761c73da
sb-sagor <sheikhsagor725@gmail.com>
2024-09-25 11:11:32 +0600

    pattern space

---
+++ pattern/pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5,i,j;
+
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if (j<n-i-1){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
